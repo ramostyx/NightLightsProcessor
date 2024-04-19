0d0bab4a6fece228b018f759e00af40fe9cecd42
ramostyx <omar.belkentaoui@etu.uae.ac.ma>
2024-04-19 18:29:36 +0100

    added monthly radiation sum calculation function

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import concurrent.futures
 import os
-import queue
+import datetime
 import subprocess
 import rasterio
-import numpy as np
 import rasterio.mask
-from rasterio.merge import merge
 import matplotlib.pyplot as plt
 import geopandas as gpd
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import contextily as cx
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 
 
 class NightLightsProcessor:
@@ -46,7 +44,17 @@ class NightLightsProcessor:
         else:
             print(f"Error executing AWS CLI command: {result.stderr}")
 
+    def filter_by_prefix(self, desired_prefix, directory_path):
+        print(f"Filtering by prefix {desired_prefix}")
 
+        command = f"aws s3 ls s3://{self.bucket_name}/{directory_path}/{desired_prefix} --recursive --no-sign-request --region {self.region}"
+        result = self.execute_cli_command(command)
+
+        if result.returncode == 0:
+            output_lines = result.stdout.strip().split('\n')
+            return [line.split()[-1] for line in output_lines]
+        else:
+            print(f"Error executing AWS CLI command: {result.stderr}")
 
     def list_directory_contents(self, directory_path):
         command = f"aws s3 ls s3://{self.bucket_name}/{directory_path}/ --no-sign-request --region {self.region}"
@@ -71,20 +79,16 @@ class NightLightsProcessor:
         else:
             print(f"Error downloading {file_key}: {result.stderr}")
 
-    def download_files(self, file_keys, destination_path="./"):
+    def download_files(self, prefix, folder_name, destination_path="./"):
         if not os.path.exists(destination_path):
             os.makedirs(destination_path)
+            command = f'aws s3 cp s3://{self.bucket_name}/{folder_name}/ {destination_path} --recursive --exclude "*" --include "{prefix}*" --no-sign-request'
+            result = self.execute_cli_command(command)
+            if result.returncode == 0:
+                print(f"All files successfully downloaded")
+            else:
+                print(f"Error downloading {result.stderr}")
 
-        # Create a comma-separated string of file keys for AWS CLI command
-        file_key_str = ",".join([f"s3://{self.bucket_name}/{file_key}" for file_key in file_keys])
-
-        command = f'aws s3 cp --request-payer=requester --recursive {file_key_str} {destination_path} --no-sign-request'
-        result = self.execute_cli_command(command)
-
-        if result.returncode == 0:
-            print(f"All files successfully downloaded")
-        else:
-            print(f"Error downloading {result.stderr}")
     @staticmethod
     def combine_tiff_files(output_file, destination_path, delete=False):
         print("Combining tiffs")
@@ -118,14 +122,14 @@ class NightLightsProcessor:
 
         return raster_polygon.intersects(region_polygon)
 
-    # Filter functions:
     def filter_file_by_region(self, file_key, region_bounds, folder_name):
-        s3_path = f"s3://{self.bucket_name}/{folder_name}/{file_key}"
+        s3_path = f"s3://{self.bucket_name}/{file_key}"
 
         with rasterio.Env(AWS_NO_SIGN_REQUEST="YES"):
             with rasterio.open(s3_path) as src:
                 if self.intersected(src, region_bounds):
-                    return s3_path
+                    raster_data = src.read(1, window=src.window(*region_bounds))
+                    return s3_path, raster_data.sum()
         return None
 
     def filter_by_region(self, object_keys, region_bounds, folder_name):
@@ -133,11 +137,11 @@ class NightLightsProcessor:
         valid_files = []
         total_files = len(object_keys)
         processed_files = 0
-        with concurrent.futures.ThreadPoolExecutor() as executor:
+        with ThreadPoolExecutor(total_files) as executor:
             future_to_key = {executor.submit(self.filter_file_by_region, file_key, region_bounds, folder_name): file_key
                              for file_key in object_keys}
 
-            for future in concurrent.futures.as_completed(future_to_key):
+            for future in as_completed(future_to_key):
                 file_key = future_to_key[future]
                 valid_file = future.result()
 
@@ -149,30 +153,13 @@ class NightLightsProcessor:
 
         return valid_files
 
-    def filter_by_prefix(self, desired_prefix, directory_path):
-        print(f"Filtering by prefix {desired_prefix}")
-        command = f"aws s3 ls s3://{self.bucket_name}/{directory_path}/ --no-sign-request --region {self.region}"
-        result = self.execute_cli_command(command)
-
-        if result.returncode == 0:
-            output_lines = result.stdout.strip().split('\n')
-            filtered_object_keys = [line.split()[-1] for line in output_lines if desired_prefix in line]
-            return filtered_object_keys
-        else:
-            print(f"Error executing AWS CLI command: {result.stderr}")
-
-    def process(self, date, region_file, product_id="SVDNB", spacecraft="npp"):
+    def process(self, date, product_id="SVDNB", spacecraft="npp"):
         folder_name = f"{spacecraft}_{date[:7].replace('-', '')}"
         prefix = f"{product_id}_{spacecraft}_d{date.replace('-', '')}"
 
-        # Filter files by region
-        object_keys = self.filter_by_prefix(prefix, folder_name)
-        region_bounds = gpd.read_file(region_file).total_bounds
-        valid_files = self.filter_by_region(object_keys, region_bounds, folder_name)
-
-        # Download and combine TIFF files
+        # Download TIFF files
         destination_path = f"./data/{prefix}"
-        self.download_files(valid_files, destination_path=destination_path)
+        self.download_files(prefix, folder_name, destination_path=destination_path)
 
         output_filename = f"{product_id}_{spacecraft}_d{date.replace('-', '')}.tif"
         output_path = os.path.join(destination_path, output_filename)
@@ -182,7 +169,7 @@ class NightLightsProcessor:
 
         return output_filename
 
-    def calculate_radiation_sum(self, date, region_file, product_id="SVDNB", spacecraft="npp"):
+    def calculate_daily_radiation_sum(self, date, region_file, product_id="SVDNB", spacecraft="npp"):
         folder_name = f"{spacecraft}_{date[:7].replace('-', '')}"
         prefix = f"{product_id}_{spacecraft}_d{date.replace('-', '')}"
 
@@ -193,17 +180,44 @@ class NightLightsProcessor:
         object_keys = self.filter_by_prefix(prefix, folder_name)
         region_bounds = gpd.read_file(region_file).total_bounds
 
-        valid_files = self.filter_by_region(object_keys, region_bounds, folder_name)
+        n_workers = 10
+        n_files = len(object_keys)
+        chunk_size = round(n_files / n_workers)
 
-        print("Done filtering")
+        with ProcessPoolExecutor(n_workers) as executor:
+            futures = list()
+            for i in range(0, n_files, chunk_size):
+                file_keys = object_keys[i:i + chunk_size]
+                future = executor.submit(self.filter_by_region, file_keys, region_bounds, folder_name)
+                futures.append(future)
 
-        for s3_path in valid_files:
-            with rasterio.Env(AWS_NO_SIGN_REQUEST="YES"):
-                with rasterio.open(s3_path) as src:
-                    raster_data = src.read(1, window=src.window(*region_bounds))
-                radiation_sum += raster_data.sum()
+            print("Done filtering")
+
+            for future in as_completed(futures):
+                valid_files = future.result()
+                for s3_path, rad_sum in valid_files:
+                    radiation_sum += rad_sum
 
         return radiation_sum
+
+    def calculate_monthly_radiation_sum(self, date, region_file, product_id="SVDNB", spacecraft="npp"):
+
+        year, month = map(int, date.split('-'))
+
+        num_days = (datetime.date(year, month + 1, 1) - datetime.date(year, month, 1)).days
+
+        monthly_radiation_sum = 0.0
+
+        with ProcessPoolExecutor(num_days) as executor:
+            futures = {executor.submit(self.calculate_daily_radiation_sum, f"{year}-{month:02d}-{day:02d}", region_file,
+                                       product_id, spacecraft): f"{year}-{month:02d}-{day:02d}" for day in
+                       range(1, num_days + 1)}
+
+            for future in as_completed(futures):
+                daily_radiation_sum = future.result()
+                monthly_radiation_sum += daily_radiation_sum
+
+        return monthly_radiation_sum
 
     @staticmethod
     def plot(ntl_file, region_file):
@@ -238,19 +252,6 @@ class NightLightsProcessor:
 # Usage example
 if __name__ == "__main__":
     processor = NightLightsProcessor(bucket_name='globalnightlight')
-    # rad_sum = processor.calculate_radiation_sum(date="2024-01-01", region_file="SA_regions.json")
-    # print("radiation_sum", rad_sum, "nanowatts/cm2/sr")
-    ntl_filename = processor.process(date="2024-01-01", region_file="SA_regions.json")
-    processor.plot(ntl_filename, region_file="SA_regions.json")
-    # Begin
-    # filtering
-    # Filtering
-    # by
-    # prefix
-    # SVDNB_npp_d20240101
-    # Filtering
-    # by
-    # region
-    # Done
-    # filtering
+    rad_sum = processor.calculate_daily_radiation_sum(date="2024-01-01", region_file="SA_regions.json")
+    print("radiation_sum", rad_sum, "nanowatts/cm2/sr")
     # radiation_sum - 9298689152.0
